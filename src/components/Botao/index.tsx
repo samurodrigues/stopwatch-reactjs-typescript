04b190fd6d517a69f81b5edad8aca71448d6afd3
Samuel S Rodrigues <samuelstievenrodrigues@gmail.com>
2023-03-01 22:29:31 -0300

    base app

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Botao.module.scss';
+
+interface IProps {
+    children: React.ReactNode;
+}
+
+class Botao extends React.Component <IProps>{
+    render() {
+        return(
+            <button className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
